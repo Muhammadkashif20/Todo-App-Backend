9cd63aa03d6f20b2839afe78ed9c3b00e6973d43
Muhammadkashif20 <154686104+Muhammadkashif20@users.noreply.github.com>
2025-08-03 11:08:22 +0500

    Complete CRUD

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT=3000;
 import "dotenv/config";
 import todoRouter from "./routers/todo.js";
 import mongoose from "mongoose";
+import cors from "cors";
 mongoose.connect(process.env.MONGODB_URI)
 .then(()=>{
     console.log(chalk.green("Connected to MongoDB successfully"));
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGODB_URI)
 .catch(()=>{
     console.log(chalk.red("Failed to connect to MongoDB"));
 })
+app.use(cors())
 app.use(express.json());
 app.use("/todo",todoRouter)
 app.listen(PORT,()=>{
